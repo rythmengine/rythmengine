0d74cf58776de1c159d900d6f33ad8facd2419be
greenlaw110 <greenlaw110@gmail.com>
2012-02-17 10:24:43 +1100

    improve @if parser error report

--- src/main/java/com/greenlaw110/rythm/internal/parser/build_in/IfParser.java
+++ src/main/java/com/greenlaw110/rythm/internal/parser/build_in/IfParser.java
@@ -1,5 +1,6 @@
 package com.greenlaw110.rythm.internal.parser.build_in;
 
+import com.greenlaw110.rythm.exception.ParseException;
 import com.greenlaw110.rythm.internal.Keyword;
 import com.greenlaw110.rythm.internal.parser.BlockCodeToken;
 import com.greenlaw110.rythm.internal.parser.ParserBase;
@@ -23,7 +24,9 @@ public class IfParser extends KeywordParserFactory {
             @Override
             public TextBuilder go() {
                 Regex r = reg(dialect());
-                if (!r.search(remain())) return null;
+                if (!r.search(remain())) {
+                    throw new ParseException(ctx().getTemplateClass(), ctx().currentLine(), "Error parsing @if statement. Correct usage: @if (some-condition) {some-template-code}");
+                }
                 String s = r.stringMatched(1);
                 ctx.step(s.length());
                 s = r.stringMatched(2);
@@ -41,7 +44,7 @@ public class IfParser extends KeywordParserFactory {
     @Override
     protected String patternStr() {
         //return "(%s(%s\\s+\\(.*\\)(\\s*\\{)?)).*";
-        return "(%s(%s\\s*" + PatternStr.Expression + "(\\s*\\n*\\s*\\{)?)).*";
+        return "(^%s(%s\\s*((?@()))(\\s*\\n*\\s*\\{)?)).*";
     }
     
     public static void main(String[] args) {
@@ -49,8 +52,9 @@ public class IfParser extends KeywordParserFactory {
         System.out.println(p);
         
         Regex r = new Regex(p);
-        String s = "@if (item) \n\t<td class=\"minus\">@item.getChange()</td>";
+        String s = "@if (hello) \n\t<td class=\"minus\">@item.getChange()</td>";
         if (r.search(s)) {
+            System.out.println(r.stringMatched());
             System.out.println(r.stringMatched(1));
             System.out.println(r.stringMatched(2));
             System.out.println(r.stringMatched(3));
