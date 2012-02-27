79b8064a06cfb6f79c87e78a796b556ea7a3a346
greenlaw110 <greenlaw110@gmail.com>
2012-02-27 21:04:51 +1100

    fix prob in HelloWorld sample app

--- samples/HelloWorld/HelloWorld.java
+++ samples/HelloWorld/HelloWorld.java
@@ -3,7 +3,7 @@ import com.greenlaw110.rythm.*;
 public class HelloWorld {
   
   public static void main(String[] args) {
-    String output = Rythm.render("hello.txt", who);
+    String output = Rythm.render("hello.txt", "world");
     System.out.println(output);
   }
 
