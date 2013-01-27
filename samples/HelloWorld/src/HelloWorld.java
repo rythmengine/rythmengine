f1e8f493dba90d10959bbb2cb4f333367f3ca5c9
Green Luo <greenlaw110@gmail.com>
2013-01-27 22:03:18 +1100

    add samples

---
+++ samples/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+import com.greenlaw110.rythm.Rythm;
+
+/**
+ * A Hello world sample
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(Rythm.render("hello @who!", "rythm"));
+    }
+}
