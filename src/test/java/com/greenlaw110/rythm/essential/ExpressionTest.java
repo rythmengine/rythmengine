b0d7158e8d565e0e4345e26193f55ee11a5f5639
Green Luo <greenlaw110@gmail.com>
2013-02-15 07:36:21 +1100

    add license header to ExpressionTest.java

--- src/test/java/com/greenlaw110/rythm/essential/ExpressionTest.java
+++ src/test/java/com/greenlaw110/rythm/essential/ExpressionTest.java
@@ -1,6 +1,24 @@
+/* 
+ * Copyright (C) 2013 The Rythm Engine project
+ * Gelin Luo <greenlaw110(at)gmail.com>
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 package com.greenlaw110.rythm.essential;
 
-import com.greenlaw110.rythm.Rythm;
 import com.greenlaw110.rythm.TestBase;
 import org.junit.Test;
 
@@ -135,7 +153,7 @@ public class ExpressionTest extends TestBase {
         // t = "@((d.nullMe() ?: new com.greenlaw110.rythm.essential.ExpressionTest.Data()).now())";
         // s = r(t, from(p("d", d)));
         // eq("foo");
-        
+
         // this will also fail, rythm cannot handle mix null safe and elvs:
 //        t = "@args com.greenlaw110.rythm.essential.ExpressionTest.Data d\n@(d?.nullMe()?:new com.greenlaw110.rythm.essential.ExpressionTest.Data())";
 //        s = r(t, from(p("d", d)));
