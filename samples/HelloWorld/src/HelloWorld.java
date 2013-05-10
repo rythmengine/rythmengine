6eda2451a34e2f45561db12c2011078347bafc21
Green Luo <greenlaw110@gmail.com>
2013-05-10 12:28:45 +1000

    update maven sample pom

--- samples/HelloWorld/src/HelloWorld.java
+++ samples/HelloWorld/src/HelloWorld.java
@@ -1,10 +1,16 @@
 import org.rythmengine.Rythm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * A Hello world sample
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(Rythm.render("@args String @1\nhello @1!", "rythm"));
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("who", "rythm");
+        System.out.println(Rythm.render("@args String @who\nhello @who!", params));
+        System.out.println(Rythm.render("@args String @who\nhello @who!", "rythm"));
     }
 }
