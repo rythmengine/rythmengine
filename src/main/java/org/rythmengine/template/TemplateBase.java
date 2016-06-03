e9d5dd6a656d826c706e5758d00ed78d8b8e5f50
Wolfgang Fahl <wf@bitplan.com>
2016-06-03 09:33:20 +0200

    improves #327

--- src/main/java/org/rythmengine/template/TemplateBase.java
+++ src/main/java/org/rythmengine/template/TemplateBase.java
@@ -1,22 +1,8 @@
 /* 
- * Copyright (C) 2013 The Rythm Engine project
- * Gelin Luo <greenlaw110(at)gmail.com>
- *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-*/
+ * Copyright (C) 2013-2016 The Rythm Engine project
+ * for LICENSE and other details see:
+ * https://github.com/rythmengine/rythmengine
+ */
 package org.rythmengine.template;
 
 import com.alibaba.fastjson.JSON;
@@ -154,7 +140,7 @@ public abstract class TemplateBase extends TemplateBuilder implements ITemplate 
     /**
      * Used for Java extension/transformer
      *
-     * @return
+     * @return the template
      */
     protected ITemplate __template() {
         return this;
