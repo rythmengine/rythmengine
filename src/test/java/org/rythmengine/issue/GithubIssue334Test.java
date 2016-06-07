e518a98a40a1d8e3e2bbac3a0b8f989cf774069b
Wolfgang Fahl <wf@bitplan.com>
2016-06-07 10:23:00 +0200

    adds testcase for #334

---
+++ src/test/java/org/rythmengine/issue/GithubIssue334Test.java
@@ -0,0 +1,30 @@
+/* 
+ * Copyright (C) 2013-2016 The Rythm Engine project
+ * for LICENSE and other details see:
+ * https://github.com/rythmengine/rythmengine
+ */
+package org.rythmengine.issue;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.rythmengine.TestBase;
+
+/**
+ * Test for https://github.com/rythmengine/rythmengine/issues/334
+ *
+ * @author wf
+ */
+public class GithubIssue334Test extends TestBase {
+
+  @Ignore
+  public void testArgsWithBlanks() throws Exception {
+  	   t = "@args() {\n" + 
+  	   		"  String name, \n" + 
+  	   		"  String firstname;\n" + 
+  	   		"}\n" + 
+  	   		"Hello @(firstname) @(name)!";
+     s = r(t,"Luo","Green");
+     eq("Hello Green Luo!");
+  }
+
+}
